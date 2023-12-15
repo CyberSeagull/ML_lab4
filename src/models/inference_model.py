5a6a544325809f52a4532f0910be43d0964c5587
Olha Chaika <62190177+CyberSeagull@users.noreply.github.com>
2023-12-15 03:40:00 +0200

    Update inference_model.py

--- src/models/inference_model.py
+++ src/models/inference_model.py
@@ -1,1 +1,33 @@
+import requests
+from io import BytesIO
+from joblib import load
+from data_processing import get_length, remove_symbols, remove_stopwords, convert_lower_case, stemming, preprocess_data
+from sklearn.ensemble import RandomForestClassifier
 
+
+def download_model(url):
+    response = requests.get(url)
+    model_file = BytesIO(response.content)
+    model = load(model_file)
+    return model
+
+
+def preprocess_input(text):
+    processed_text = text.preprocess_data
+    return processed_text
+
+
+def predict_text(text, model):
+    processed_text = preprocess_input(text)
+    prediction = model.predict([processed_text])
+    return prediction
+
+
+model_url = 'https://github.com/CyberSeagull/ML_lab4/blob/main/src/models/random_forest_model_weights.joblib'
+
+rand_forest_model = download_model(model_url)
+
+
+text = input("Enter your text: ")
+prediction = predict_text(text, rand_forest_model)
+print("Prediction:", prediction)
